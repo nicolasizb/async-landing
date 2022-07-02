e9aeab1dbb31f74199b26a7436766935cdb89754
nicolasizquierdo <nicolasizquierdocontacto@gmail.com>
2022-07-01 23:25:06 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,46 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCCB-Jr75S_Bu7RbZlsgEYgA&part=snippet%2Cid&order=date&maxResults=5';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '3fcfdc64dcmsh4135e5a33e1e396p12d498jsn0815d4aedc70',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fecthData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+// FUNCION ANÓNIMA, se ejecuta inmediatamente 
+(async () => {
+    try {
+        const videos = await fecthData(API);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                        <span aria-hidden="true" class="absolute inset-0"></span>
+                        ${video.snippet.title}
+                    </h3>
+                </div>
+            </div>
+        `).slice(0,4).join('')}
+        `;
+
+        content.innerHTML = view;
+
+    } catch (err) {
+        console.error(err);
+        alert(err)
+    }
+})();
